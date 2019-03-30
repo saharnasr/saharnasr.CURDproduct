5885a2839348895c621d8de4c485b5e91c964d2c
sahar <saharnasr690@yahoo.com>
2019-03-30 03:46:41 +0200

    my first commit

---
+++ ProductMangement/ProductMangement/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProductMangement.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Product Name")]
+        public string title { get; set; }
+        [DisplayName ( "Category")]
+        public string description { get; set; }
+        public int price { get; set; }
+    }
+}
